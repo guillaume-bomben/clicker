0e1e6a7e5a247d265df8fe842e10723119140d09
Abakar-Issa-Ali <abakar-issa-ali@laplateforme.io>
2024-04-23 19:34:54 +0200

    [Added] added spawnFunction for bouns button when the progress-bar reached 100%

--- script.js
+++ script.js
@@ -10,22 +10,22 @@ var decreaseInterval;
 var isMouseDown = false;
 
 $(document).ready(function() {
-   const button = $("#button");
-   const score = $("#score");
-    const counterDisplay = $("#counterLevel"); 
-    const spawn = $("#spawn"); 
+    const button = $("#button");
+    const score = $("#score");
+    const counterDisplay = $("#counterLevel");
+    const spawn = $("#spawn");
     const bar = $("#bar");
 
-    button.mousedown(function() {              // Function to handle mouse down event on the button
+    button.mousedown(function() { // Function to handle mouse down event on the button
         isMouseDown = true;
-        clearInterval(decreaseInterval);          // Clear any ongoing decrease interval
+        clearInterval(decreaseInterval); // Clear any ongoing decrease interval
         interval = setInterval(function() {
             clicksPerSecond++;
             if (isMouseDown) {
                 increaseCounterLevel();
             }
         }, 1000);
-       
+
         clickCounter++;
         var scoreNumber = parseInt(score.text());
         scoreNumber += moneyPerClick;
@@ -38,17 +38,16 @@ $(document).ready(function() {
         setTimeout(function() {
             button.removeClass('animate');
         }, 150);
-    });    
-        
-    button.mouseup(function() {                  // Function to handle mouse up event on the button
-        isMouseDown = false;
-        clearInterval(interval);            // Clear the interval for continuous increase
-        startDecreaseInterval();
-        increaseCounterLevel();  
-        clicksPerSecond = 0;
-
     });
-     
+
+    button.mouseup(function() { // Function to handle mouse up event on the button
+        isMouseDown = false;
+        clearInterval(interval); // Clear the interval for continuous increase
+        startDecreaseInterval();
+        increaseCounterLevel();
+        clicksPerSecond = 0;
+    });
+
     $("#upgrade").click(function() {
         if (money >= price) {
             money -= price;
@@ -59,7 +58,7 @@ $(document).ready(function() {
         }
     });
 
-    function startDecreaseInterval() {          // Function to start the decrease interval for counter level
+    function startDecreaseInterval() { // Function to start the decrease interval for counter level
         decreaseInterval = setInterval(function() {
             if (counterLevel > 0) {
                 counterLevel -= 1;
@@ -67,39 +66,42 @@ $(document).ready(function() {
             } else {
                 clearInterval(decreaseInterval);
                 introduceBonusButtons();
-            
             }
-        }, 100); 
-            
+        }, 100);
     }
 
     function updateProgressBar() {
         bar.css('width', counterLevel + '%');
+        if (counterLevel >= 100) {
+            spawnFunction(); // Call spawn function when counterLevel reaches 100
+        }
     }
 
-    function increaseCounterLevel() {         // Function to increase the counter level
+    function increaseCounterLevel() { // Function to increase the counter level
         if (counterLevel < 100) {
             counterLevel += 2;
             updateProgressBar();
         }
-        
     }
-    
 
-});
-$("#spawn").click(function() {
-    $(".small_button").remove();
-    for (let i = 0; i < 4; i++) {
-        const randomX = Math.floor(Math.random() * ($(window).width() - 100));
-        const randomY = Math.floor(Math.random() * ($(window).height() - 100));
-        let smallButton = $("<img>").addClass("small_button").attr("src", "Images/Small_button.svg").css({ top: randomY, left: randomX, position: "absolute", width: "100px", height: "100px"});
-        $("body").append(smallButton);
-        smallButton.click(function() {
-            money += moneyPerClick*15;
-            totalMoney += moneyPerClick*15;
-            $("#money").text(money + " $");
-            $(this).remove();
-    
-        });
+    // Define spawn function
+    function spawnFunction() {
+        $(".small_button").remove();
+        if( counterLevel === 100){
+            counterLevel === startDecreaseInterval();
+        }
+        updateProgressBar();
+        for (let i = 0; i < 4; i++) {
+            const randomX = Math.floor(Math.random() * ($(window).width() - 100));
+            const randomY = Math.floor(Math.random() * ($(window).height() - 100));
+            let smallButton = $("<img>").addClass("small_button").attr("src", "Images/Small_button.svg").css({ top: randomY, left: randomX, position: "absolute", width: "100px", height: "100px" });
+            $("body").append(smallButton);
+            smallButton.click(function() {
+                money += moneyPerClick * 15;
+                totalMoney += moneyPerClick * 15;
+                $("#money").text(money + " $");
+                $(this).remove();
+            });
+        }
     }
 });
