6454104a55ec5370514eed2af44a2ecdf97fce4b
baptiste-appriou <baptiste.appriou@laplateforme.io>
2024-04-23 13:59:24 +0200

    [creation] save protocol on each action + random spawn of small buttons

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ var OMEGAPRICE = 1000;
 
 $(document).ready(function() {
     const button = $("#button");
-
+    save();
     button.click(function() {
         money += moneyPerClick;
         clickCounter++;
@@ -20,6 +20,7 @@ $(document).ready(function() {
         setTimeout(function() {
             button.removeClass('animate'); // remove the 'animate' class after 0.5s
         }, 150);
+        save();
     });
 
     $("#stats").click(function() {
@@ -35,6 +36,7 @@ $(document).ready(function() {
             $("#money").text(money + " $");
             $("#moneyPerClick").text(moneyPerClick + " $/click");
         }
+        save();
     });
 
     $("#upgrade_clicker_button").click(function() {
@@ -49,5 +51,34 @@ $(document).ready(function() {
         else {
             alert("You poor XD");
         }
+        save();
     });
+
+    $("#spawn").click(function() {
+        $(".small_button").remove();
+        for (let i = 0; i < 4; i++) {
+            const randomX = Math.floor(Math.random() * ($(window).width() - 100));
+            const randomY = Math.floor(Math.random() * ($(window).height() - 100));
+            let smallButton = $("<img>").addClass("small_button").attr("src", "Images/Small_button.svg").css({ top: randomY, left: randomX, position: "absolute", width: "100px", height: "100px"});
+            $("body").append(smallButton);
+            smallButton.click(function() {
+                money += moneyPerClick*15;
+                totalMoney += moneyPerClick*15;
+                $("#money").text(money + " $");
+                $(this).remove();
+                save();
+            });
+        }
+    });
+    
 });
+
+async function save() {
+    localStorage.setItem("money", money);
+    localStorage.setItem("totalMoney", totalMoney);
+    localStorage.setItem("totalSpend", totalSpend);
+    localStorage.setItem("moneyPerClick", moneyPerClick);
+    localStorage.setItem("clickCounter", clickCounter);
+    localStorage.setItem("price", price);
+    localStorage.setItem("OMEGAPRICE", OMEGAPRICE);
+};
