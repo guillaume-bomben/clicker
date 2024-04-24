082915ca277de510112581592ae96281c7cf573a
baptiste-appriou <baptiste.appriou@laplateforme.io>
2024-04-24 11:59:00 +0200

    [change] Merged the buttons correctly

--- script.js
+++ script.js
@@ -1,32 +1,98 @@
 var money = 0;
+var totalMoney = 0;
+var totalSpend = 0;
 var moneyPerClick = 1;
 var clickCounter = 0;
 var price = 10;
+var OMEGAPRICE = 1000;
 
 $(document).ready(function() {
     const button = $(".Big_button");
-    const score = $("#score");
-
+    save();
     button.click(function() {
-        var scoreNumber = parseInt(score.text());
-        scoreNumber += moneyPerClick;
         money += moneyPerClick;
-        score.text(scoreNumber);
-        $("#money").text(money);
-        $("#moneyPerClick").text(moneyPerClick);
+        clickCounter++;
+        totalMoney += moneyPerClick;
+        // $("#money").text(money + " $");
+        // $("#moneyPerClick").text(moneyPerClick + " $/click");
+        show_money();
         $(this).addClass('animate');
         $(this).one('animationend', function() {
             $(this).removeClass('animate');
         });
+        save();
     });
 
     $(".increase_income").click(function() {
         if (money >= price) {
             money -= price;
+            totalSpend += price;
             price += 10;
-            moneyPerClick += 1;
-            $("#money").text(money);
-            $("#moneyPerClick").text(moneyPerClick);
+            if (price > 100) {
+                moneyPerClick = moneyPerClick * 1.2;
+            }
+            else {
+                moneyPerClick += 1;
+            }
+            show_money();
         }
+        save();
+    });
+    
+    $(".statistics_button").click(function() {
+        alert("Total money earned: " + totalMoney + "$\n" + "Total money spend: " + totalSpend + "$\n" + "Total clicks: " + clickCounter)
+    });
+
+    $(".upgrade_button").click(function() {
+        if (money >= OMEGAPRICE) {
+            money -= OMEGAPRICE;
+            totalSpend += OMEGAPRICE;
+            OMEGAPRICE += OMEGAPRICE*100;
+            moneyPerClick += moneyPerClick*20;
+            price += price*20;
+            $("#money").text(money + " $");
+            $("#moneyPerClick").text(moneyPerClick + " $/click");
+        }
+        else {
+            alert("You poor XD");
+        }
+        save();
     });
 });
+
+async function save() {
+    localStorage.setItem("money", money);
+    localStorage.setItem("totalMoney", totalMoney);
+    localStorage.setItem("totalSpend", totalSpend);
+    localStorage.setItem("moneyPerClick", moneyPerClick);
+    localStorage.setItem("clickCounter", clickCounter);
+    localStorage.setItem("price", price);
+    localStorage.setItem("OMEGAPRICE", OMEGAPRICE);
+};
+
+async function spawn(){
+    $(".small_button").remove();
+        for (let i = 0; i < 4; i++) {
+            const randomX = Math.floor(Math.random() * ($(window).width() - 100));
+            const randomY = Math.floor(Math.random() * ($(window).height() - 100));
+            let smallButton = $("<img>").addClass("small_button").attr("src", "Images/Small_button.svg").css({ top: randomY, left: randomX, position: "absolute", width: "100px", height: "100px"});
+            $("body").append(smallButton);
+            smallButton.click(function() {
+                money += moneyPerClick*15;
+                totalMoney += moneyPerClick*15;
+                // $("#money").text(money + " $");
+                $(this).remove();
+                save();
+            });
+        }
+}
+
+function show_money(){
+    let money_to_show = money;
+    let moneyPerClick_to_show = moneyPerClick;
+    if (money_to_show > 1000) {
+        money_to_show = (money_to_show / 1000) + "k";
+    }
+    $("#money").text(money_to_show.toFixed(1) + " $");
+    $("#moneyPerClick").text(moneyPerClick_to_show.toFixed(1) + " $/click");
+}
